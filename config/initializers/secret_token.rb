7ada39ad92c819ef7caddeecbb8a1ba01680b4e5
Valentine Ollawa <vollawa@gmail.com>
2013-12-17 11:03:07 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookclub::Application.config.secret_key_base = '76003e6b50c7b7c521d18ad111bb8a9ff9afaab2447e4c65d0fd6d1cbb2fd7c123d8f4b3dd36c25bbd1f551bfdadcdc31e345938aba1c8b1903dd9042f663441'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bookclub::Application.config.session_store :cookie_store, key: '_Bookclub_session'
