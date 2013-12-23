edd1a771acfac1ad06a097114709dfeeaec0312e
Valentine Ollawa <vollawa@gmail.com>
2013-12-22 23:11:41 -0800

    setting up books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,65 +1,42 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
-  # GET /books
-  # GET /books.json
   def index
     @books = Book.all
   end
 
-  # GET /books/1
-  # GET /books/1.json
   def show
   end
 
-  # GET /books/new
   def new
     @book = Book.new
   end
 
-  # GET /books/1/edit
-  def edit
+   def edit
   end
 
-  # POST /books
-  # POST /books.json
-  def create
+   def create
     @book = Book.new(book_params)
 
-    respond_to do |format|
       if @book.save
-        format.html { redirect_to @book, notice: 'Book was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @book }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
+       redirect_to @book, notice: 'Book was successfully created.' 
+       else
+        render action: 'new' 
       end
-    end
   end
 
-  # PATCH/PUT /books/1
-  # PATCH/PUT /books/1.json
-  def update
-    respond_to do |format|
+   def update
       if @book.update(book_params)
-        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
-        format.json { head :no_content }
+       redirect_to @book, notice: 'Book was successfully updated.' 
       else
-        format.html { render action: 'edit' }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
+       render action: 'edit' 
       end
     end
-  end
 
-  # DELETE /books/1
-  # DELETE /books/1.json
   def destroy
     @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url }
-      format.json { head :no_content }
+     redirect_to books_url 
     end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
