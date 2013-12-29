e0f9dfd01b99a6d22b0281ea0d0cf4506b19b213
Valentine Ollawa <vollawa@gmail.com>
2013-12-28 16:46:58 -0800

    add names to users

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,8 @@ class Book < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+
+validates :description, presence: true
+validates :image, presence: true
 end
 
